40b26a860c470d1664158bb620e403d6b4cfe9ad
1414504 <1414504@rgu.ac.uk>
2016-08-16 16:52:44 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shahidbaig
+ * Date: 16/08/16
+ * Time: 4:33 PM
+ */
+?>
+<html>
+<head>
+    <title>
+        Azur Test
+    </title>
+    <body>
+<h1>WELCOM TO AZURE FIRST TEST FILE</h1>
+</body>
+</head>
+</html>
